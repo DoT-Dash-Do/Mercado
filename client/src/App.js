390ee461c28dc5282ab630c62881f15150f439ac
Saksham Kamboj <sakshamkamboj71@gmail.com>
2024-04-11 12:47:33 +0530

    branch check

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline text-center">
-        America yaa halo halo halo
+        America yaa halo halo halo halo halo halo halo halo halo halo halo halo
       </h1>
     </div>
   );
