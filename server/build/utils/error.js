90b13982bd5dc4be7bf0cc7601c61b36581f65b4
kcoolgamer26@gmail.com <kcoolgamer26@gmail.com>
2024-05-08 22:51:54 +0530

    Revert "Revert "check 1""
    This reverts commit 31e922c9235887cf88a98522befad6ff676d71c6.

--- server/build/utils/error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const customError_1 = require("../types/customError");
-const errorHandler = (statusCode, message) => {
-    return new customError_1.CustomError(statusCode, message);
-};
-exports.errorHandler = errorHandler;
